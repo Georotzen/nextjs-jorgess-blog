186a65aac54bd455bdac4c2815311d5aa86887fb
Jorge S. Sanchez <georotzen@gmail.com>
2024-11-17 15:47:37 -0800

    Install Next.JS, Tailwind CSS, and Clerk Auth.

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='text-blue-500 text-center'>Software Engineering 2024 - Frontend Development NestJS - Introduction</div>
+  )
+}
